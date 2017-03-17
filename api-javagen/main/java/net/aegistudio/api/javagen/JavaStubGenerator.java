214043993b4c9f39c27de4b20063b3159527825c
aegistudio <1910567646@qq.com>
2017-03-18 04:54:42 +0800

    Fix.

---
+++ api-javagen/main/java/net/aegistudio/api/javagen/JavaStubGenerator.java
@@ -0,0 +1,15 @@
+package net.aegistudio.api.javagen;
+
+import java.io.IOException;
+
+import net.aegistudio.api.Document;
+import net.aegistudio.api.gen.Context;
+import net.aegistudio.api.gen.Generator;
+
+public class JavaStubGenerator implements Generator {
+	
+	@Override
+	public void generate(Context context, Document dom) throws IOException {
+		
+	}
+}
